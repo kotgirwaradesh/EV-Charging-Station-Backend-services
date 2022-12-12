12f3e8cfcc29a19175c55bbf9ebd59fd0eb84959
adeshkotgirwar <kotgirwaradesh3@gmail.com>
2022-12-12 13:10:38 +0530

    final

---
+++ src/main/java/com/javatechie/jwt/api/entity/User.java
@@ -0,0 +1,59 @@
+package com.javatechie.jwt.api.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "USER_TBL")
+public class User {
+    @Id
+    private int id;
+    private String userName;
+    private String password;
+    private String email;
+    
+	public User() {
+		super();
+	}
+	public User(int id, String userName, String password, String email) {
+		super();
+		this.id = id;
+		this.userName = userName;
+		this.password = password;
+		this.email = email;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+    
+    
+}
